bf5f046198045f156f212d8545b1082cc2984ee2
Mateusz <mat.sek@wp.pl>
2017-11-19 08:22:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function set(){
+    let elements = document.getElementsByName("cssProperty");
+    for (let i=0;i<elements.length;i++){
+        document.getElementById("modify").style.cssText+=elements[i].getAttribute("id")+":"+elements[i].value;
+    }
+}
+document.getElementById("but1").addEventListener("click", set);
