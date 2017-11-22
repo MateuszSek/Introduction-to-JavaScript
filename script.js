0c536f49c26a34d3644d617b775dd1953cec109b
Mateusz <mat.sek@wp.pl>
2017-11-22 22:03:01 +0100

    Update script.js
    adding change event (instead of keeping 'onchange' in HTML file)
    adding function main

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function set(){
-    let elements = document.getElementsByName("cssProperty");
-    for (let i=0;i<elements.length;i++){
-        document.getElementById("modify").style.cssText+=elements[i].getAttribute("id")+":"+elements[i].value;
+$(document).ready(main);
+function main(){
+    function set(){
+        let elements = document.getElementsByName("cssProperty");
+        for (let i=0;i<elements.length;i++){
+            document.getElementById("modify").style.cssText+=elements[i].getAttribute("id")+":"+elements[i].value;
+        }
     }
+    document.getElementById("but1").addEventListener("click", set);
+    $("select").change(set);
 }
-document.getElementById("but1").addEventListener("click", set);
